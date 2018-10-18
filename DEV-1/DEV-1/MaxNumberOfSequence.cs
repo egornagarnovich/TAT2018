beeeff2dd1ee1ef56c309b9caba0c68e262f09c3
egornagarnovich <egor.naganovich@gmail.com>
2018-10-18 13:10:14 +0300

    Add DEV-1.sln, DEV-1.csproj

---
+++ DEV-1/DEV-1/MaxNumberOfSequence.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace DEV1
+{
+	/// <summary>
+    /// The MaxNumberOfSequence class.
+	/// Contains all methods for performing basic count functions.
+    /// </summary>
+    public class MaxNumberOfSequence
+    {
+		/// <summary>
+		/// Method accepts a string line of different symbols from command line.
+		/// Return: Maximal sequence of different symbols.
+		/// </summary>
+		public static int GetLengthOfSequence(string inputString)
+        {
+            if (inputString.Length == 1)
+				return inputString.Length;
+			int maxLength = 1;
+            Stack<char> setOfUniqueVariables = new Stack<char>();
+            for (int i = 0; i < inputString.Length; i++)
+            {
+				setOfUniqueVariables.Push(inputString[i]);
+                for (int j = i + 1; (j < inputString.Length) &&
+				     (setOfUniqueVariables.Contains(inputString[j]) == false); j++)
+                {
+					setOfUniqueVariables.Push(inputString[j]);
+					if (setOfUniqueVariables.Count > maxLength)
+                    {
+						maxLength = setOfUniqueVariables.Count;
+                    }
+                }
+				setOfUniqueVariables.Clear();
+            }
+            return maxLength;
+        }
+    }
+}
