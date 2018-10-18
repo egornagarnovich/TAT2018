499e9750aa31e389947a244710e091ae96604282
egornagarnovich <egor.naganovich@gmail.com>
2018-10-18 13:28:27 +0300

    Fix cod style

--- DEV-1/DEV-1/MaxNumberOfSequence.cs
+++ DEV-1/DEV-1/MaxNumberOfSequence.cs
@@ -2,35 +2,35 @@
 
 namespace DEV1
 {
-	/// <summary>
+    /// <summary>
     /// The MaxNumberOfSequence class.
-	/// Contains all methods for performing basic count functions.
+    /// Contains all methods for performing basic count functions.
     /// </summary>
     public class MaxNumberOfSequence
     {
-		/// <summary>
-		/// Method accepts a string line of different symbols from command line.
-		/// Return: Maximal sequence of different symbols.
-		/// </summary>
-		public static int GetLengthOfSequence(string inputString)
+	/// <summary>
+	/// Method accepts a string line of different symbols from command line.
+	/// Return: Maximal sequence of different symbols.
+	/// </summary>
+	public static int GetLengthOfSequence(string inputString)
         {
             if (inputString.Length == 1)
-				return inputString.Length;
-			int maxLength = 1;
+		return inputString.Length;
+	    int maxLength = 1;
             Stack<char> setOfUniqueVariables = new Stack<char>();
             for (int i = 0; i < inputString.Length; i++)
             {
-				setOfUniqueVariables.Push(inputString[i]);
+		setOfUniqueVariables.Push(inputString[i]);
                 for (int j = i + 1; (j < inputString.Length) &&
-				     (setOfUniqueVariables.Contains(inputString[j]) == false); j++)
+		     (setOfUniqueVariables.Contains(inputString[j]) == false); j++)
                 {
-					setOfUniqueVariables.Push(inputString[j]);
-					if (setOfUniqueVariables.Count > maxLength)
+		    setOfUniqueVariables.Push(inputString[j]);
+		    if (setOfUniqueVariables.Count > maxLength)
                     {
-						maxLength = setOfUniqueVariables.Count;
+			maxLength = setOfUniqueVariables.Count;
                     }
                 }
-				setOfUniqueVariables.Clear();
+		setOfUniqueVariables.Clear();
             }
             return maxLength;
         }
