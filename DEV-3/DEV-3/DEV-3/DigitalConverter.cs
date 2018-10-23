008cc7bc432f790dc393794f2ea5e17d55c1c5e0
egornagarnovich <egor.naganovich@gmail.com>
2018-10-23 20:40:04 +0300

    fix cod style

--- DEV-3/DEV-3/DEV-3/DigitalConverter.cs
+++ DEV-3/DEV-3/DEV-3/DigitalConverter.cs
@@ -2,29 +2,29 @@
 using System.Text;
 namespace DEV3
 {
-	/// <summary>
+    /// <summary>
     /// The Digital converter.
-	/// Contains all methods for performing basic conversion functions.
+    /// Contains all methods for performing basic conversion functions.
     /// </summary>
     public class DigitalConverter
     {
-		/// <summary>
+	/// <summary>
         /// Converts to different digital systems.
         /// </summary>
         /// <returns>The to different digital systems.</returns>
         /// <param name="number">Number.</param>
         /// <param name="convertedNumber">Digital system.</param>
-		public string ConvertToDifferentDigitalSystems(int number, int convertedNumber)
+	public string ConvertToDifferentDigitalSystems(int number, int convertedNumber)
         {
-			string controlValues = "0123456789ABCDEFGHIJ";
-			StringBuilder numberInNewSystem = new StringBuilder(); 
-			do
-			{
-				int numberToAdd = number % convertedNumber;
-				numberInNewSystem.Insert(0, controlValues[Math.Abs(numberToAdd)]);
-				number = number / convertedNumber;
-			} while (number != 0);
-			return numberInNewSystem.ToString();
+	string controlValues = "0123456789ABCDEFGHIJ";
+	StringBuilder numberInNewSystem = new StringBuilder(); 
+	do
+	{
+	    int numberToAdd = number % convertedNumber;
+	    numberInNewSystem.Insert(0, controlValues[Math.Abs(numberToAdd)]);
+	    number = number / convertedNumber;
+	} while (number != 0);
+	  return numberInNewSystem.ToString();
         }
     }
 }
