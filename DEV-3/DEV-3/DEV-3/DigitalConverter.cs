fa326f235db5ff7b3d255422ca640ab1eb6bdff3
egornagarnovich <egor.naganovich@gmail.com>
2018-10-23 11:05:06 +0300

    add new program

---
+++ DEV-3/DEV-3/DEV-3/DigitalConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DEV3
+{
+	/// <summary>
+    /// The Digital converter.
+	/// Contains all methods for performing basic conversion functions.
+    /// </summary>
+    public class DigitalConverter
+    {
+		/// <summary>
+        /// Converts to different digital systems.
+        /// </summary>
+        /// <returns>The to different digital systems.</returns>
+        /// <param name="number">Number.</param>
+        /// <param name="convertedNumber">Digital system.</param>
+		public string ConvertToDifferentDigitalSystems(int number, int convertedNumber)
+        {
+			return Convert.ToString(number, convertedNumber);
+        }
+    }
+}
