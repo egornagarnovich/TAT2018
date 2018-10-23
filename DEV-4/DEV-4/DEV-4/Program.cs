0d54c5dd10c4b7371c243cf0d06597eaa309f2ad
egornagarnovich <egor.naganovich@gmail.com>
2018-10-23 19:40:18 +0300

    add xml file, add file reader program

---
+++ DEV-4/DEV-4/DEV-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace DEV4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			try
+			{
+				string pathToXmlFile = args[0];
+				XmlParser parser = new XmlSorter();
+				string dataXml = File.ReadAllText(pathToXmlFile);
+				string parsedXml = parser.ParseXml(ref dataXml);
+                Console.WriteLine(parsedXml);
+			}
+			catch(Exception error)
+			{
+				Console.WriteLine("Error: " + error.Message);
+			}
+        }
+    }
+}
