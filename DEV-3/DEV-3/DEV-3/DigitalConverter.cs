e5ceee9125648d46e797c3d1352a433cffd37eb2
egornagarnovich <egor.naganovich@gmail.com>
2018-10-24 11:14:12 +0300

    fix cod style

--- DEV-3/DEV-3/DEV-3/DigitalConverter.cs
+++ DEV-3/DEV-3/DEV-3/DigitalConverter.cs
@@ -8,23 +8,23 @@ namespace DEV3
     /// </summary>
     public class DigitalConverter
     {
-	/// <summary>
+        /// <summary>
         /// Converts to different digital systems.
         /// </summary>
         /// <returns>The to different digital systems.</returns>
         /// <param name="number">Number.</param>
         /// <param name="convertedNumber">Digital system.</param>
-	public string ConvertToDifferentDigitalSystems(int number, int convertedNumber)
+        public string ConvertToDifferentDigitalSystems(int number, int convertedNumber)
         {
-	string controlValues = "0123456789ABCDEFGHIJ";
-	StringBuilder numberInNewSystem = new StringBuilder(); 
-	do
-	{
-	    int numberToAdd = number % convertedNumber;
-	    numberInNewSystem.Insert(0, controlValues[Math.Abs(numberToAdd)]);
-	    number = number / convertedNumber;
-	} while (number != 0);
-	  return numberInNewSystem.ToString();
+            string controlValues = "0123456789ABCDEFGHIJ";
+            StringBuilder numberInNewSystem = new StringBuilder(); 
+            do
+            {
+                int numberToAdd = number % convertedNumber;
+                numberInNewSystem.Insert(0, controlValues[Math.Abs(numberToAdd)]);
+                number = number / convertedNumber;
+            } while (number != 0);
+            return numberInNewSystem.ToString();
         }
     }
 }
--- DEV-3/DEV-3/DEV-3/Program.cs
+++ DEV-3/DEV-3/DEV-3/Program.cs
@@ -2,31 +2,31 @@
 
 namespace DEV3
 {
-	/// <summary>
+    /// <summary>
     /// The Main class.
-	/// Contains all methods for performing basic functions.
+    /// Contains all methods for performing basic functions.
     /// </summary>
     class MainClass
     {
-		/// <summary>
+        /// <summary>
         /// The entry point of the program, where the program control starts and ends.
         /// </summary>
         /// <param name="args">The command-line arguments.</param>
         public static void Main(string[] args)
         {
-			try
-			{
-				int number = int.Parse(args[0]);
+            try
+            {
+                int number = int.Parse(args[0]);
                 int newDigitalSystem = int.Parse(args[1]);
-				if (newDigitalSystem < 2 || newDigitalSystem > 20)
-				{
-					throw new Exception("Incorrect of argument.");
-				}
-				DigitalConverter converter = new DigitalConverter();
+                if (newDigitalSystem < 2 || newDigitalSystem > 20)
+                {
+                    throw new Exception("Incorrect of argument.");
+                }
+                DigitalConverter converter = new DigitalConverter();
                 string convertedNumber = converter.ConvertToDifferentDigitalSystems(number, newDigitalSystem);
                 Console.WriteLine(convertedNumber);
-			}
-			catch (Exception error)
+            }
+            catch (Exception error)
             {
                 Console.WriteLine("Error: " + error.Message);
             }
