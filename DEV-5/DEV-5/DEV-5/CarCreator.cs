2544f06e7336efe0211bc87dbfc4f08c4bbd09db
egornagarnovich <egor.naganovich@gmail.com>
2018-11-03 16:23:59 +0300

    crete new program

---
+++ DEV-5/DEV-5/DEV-5/CarCreator.cs
@@ -0,0 +1,22 @@
+﻿namespace DEV5
+{
+    /// <summary>
+    /// Car creator class.
+    /// </summary>
+    public class CarCreator
+    {
+        DEV5.Car car = Car.GetObjCar();
+        DEV5.Interviewer interviewer = Interviewer.GetObjInterviewer();
+
+        /// <summary>
+        /// Creats new car.
+        /// </summary>
+        public void Create()
+        {
+            car.Brand = interviewer.AskBrandOfCars();
+            car.Model = interviewer.AskModelOfCars();
+            car.Quantity = interviewer.AskQuantityOfCars();
+            car.CostPerUnit = interviewer.AskCostPerUnitOfCar();
+        }
+    }
+}
---
+++ DEV-5/DEV-5/DEV-5/DatabaseInformation.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+namespace DEV5
+{
+    /// <summary>
+    /// Contains methods for retrieving information from the database.
+    /// </summary>
+    public class DatabaseInformation
+    {
+        private DEV5.DatabaseOfCars objDatabase = DatabaseOfCars.GetDatabase();
+        /// <summary>
+        /// Reads database fields. Counts data.
+        /// </summary>
+        /// <returns>Car brand list.</returns>
+        public List<string> GetAllBrands()
+        {
+            List<string> brands = new List<string>();
+            foreach (string brand in objDatabase.Database.Keys)
+            {
+                brands.Add(brand);
+            }
+            return brands;
+        }
+
+        /// <summary>
+        /// Reads database fields. Counts data.
+        /// </summary>
+        /// <returns>The the sum of car of the cost per unit.</returns>
+        public int getTheSumOfTheCostPerUnit()
+        {
+            int countOfCostPerUnit = 0;
+            foreach (var elementValues in objDatabase.Database.Values)
+            {
+                foreach (var element in elementValues.Values)
+                {
+                    countOfCostPerUnit += element[1];
+                }
+            }
+            return countOfCostPerUnit;
+        }
+
+        /// <summary>
+        /// Reads database fields. Counts data.
+        /// </summary>
+        /// <returns>The amount of the cost of a particular car brand  per  unit.</returns>
+        /// <param name="brand">Brand of car.</param>
+        public int getTheSumOfTheCostPerUnit(string brand)
+        {
+            int countOfCostPerUnit = 0;
+            foreach (KeyValuePair<string, Dictionary<string, List<int>>> pair in objDatabase.Database)
+            {
+                if (pair.Key == brand)
+                {
+                    foreach (var elementValues in pair.Value)                        
+                    {
+                        countOfCostPerUnit += elementValues.Value[1];
+                    }
+                }
+            }
+            return countOfCostPerUnit;
+        }
+
+        /// <summary>
+        /// Rads database fields. Add data to list.
+        /// </summary>
+        /// <returns>List of cars of each brand.</returns>
+        public List<int> GetOfQuantityAllCars()
+        {
+            List<int> listOfCars = new List<int>();
+            foreach (var elementValues in objDatabase.Database.Values)
+            {
+                foreach (var element in elementValues.Values)
+                {
+                    listOfCars.Add(element[0]);
+                }
+            }
+            return listOfCars;
+        }
+
+        /// <summary>
+        /// Rads database fields. Add data to list.
+        /// </summary>
+        /// <returns>List of cars of a particular brand.</returns>
+        /// <param name="brand">Brand of car.</param>
+        public List<int> GetOfQuantityAllCars(string brand)
+        {
+            List<int> listOfCars = new List<int>();
+            foreach (KeyValuePair<string, Dictionary<string, List<int>>> pair in objDatabase.Database)
+            {
+                if (pair.Key == brand)
+                {
+                    foreach (var elementValues in pair.Value)
+                    {
+                        listOfCars.Add(elementValues.Value[0]);
+                    }
+                }
+            }
+            return listOfCars;
+        }
+    }
+}
