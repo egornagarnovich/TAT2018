bbe121551f5c87861d5914cd01c39ffb1af0a5ce
egornagarnovich <egor.naganovich@gmail.com>
2018-12-02 13:11:20 +0300

    Add new program

---
+++ DEV-8/DEV-8/DEV-8/ICommand.cs
@@ -0,0 +1,11 @@
+﻿namespace DEV8
+{
+    /// <summary>
+    /// The abstract command defines a common interface for specific
+    /// classes of commands and contains the basic behavior of the cancel operation.
+    /// </summary>
+    abstract class ICommand
+    {
+        public abstract void Execute();
+    }
+}
---
+++ DEV-8/DEV-8/DEV-8/Interviewer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DEV8
+{
+    /// <summary>
+    /// Class containing user interaction methods
+    /// </summary>
+    public class Interviewer
+    {
+        private List<string> trueCommands = new List<string> { "count types", "count all", "average price", "execute" };
+
+        /// <summary>
+        /// Asks the user command.
+        /// Checks whether the brand is entered correctly.
+        /// </summary>
+        /// <returns>The user command.</returns>
+        public string AskCommand()
+        {
+            Console.WriteLine("Enter command: ");
+            string inputCommand = Console.ReadLine();
+            if (!trueCommands.Contains(inputCommand))
+            {
+                Console.WriteLine("Sorry, Wrong command. Try again.");
+                AskCommand();
+            }
+            return inputCommand;
+        }
+    }
+}
