f1a60706949093b6604d98844f824572812dc705
egornagarnovich <egor.naganovich@gmail.com>
2018-12-09 14:19:09 +0300

    add new program

---
+++ work2/task2/task2/LinkDetector.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+namespace task2
+{
+    public class LinkDetector
+    {
+        public string GetLink(ref string inputString)
+        {
+            LinkValidation validator = new LinkValidation();
+            string baseLink = "https://";
+            StringBuilder link = new StringBuilder();
+            if (!inputString.Contains(baseLink))
+            {
+                return "No link.";
+            }
+            int index = inputString.IndexOf(baseLink) + baseLink.Length;
+            link.Append(baseLink);
+            bool startAndStopMarker = false;
+            while(startAndStopMarker != true && index != inputString.Length)
+            {
+                link.Append(inputString[index]);
+                startAndStopMarker = validator.IsValidUri(link.ToString());
+                index++;
+            }
+            if (startAndStopMarker == false)
+                return "No valid link. " + link.ToString();
+            return link.ToString();
+        }
+    }
+}
---
+++ work2/task2/task2/LinkValidation.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System;
+namespace task2
+{
+    public class LinkValidation
+    {
+        public bool IsValidUri(string link)
+        {            
+            try
+            {
+                Console.WriteLine('q');
+
+                WebClient wc = new WebClient();
+                string HTMLSource = wc.DownloadString(link);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }          
+        }
+    }
+}
